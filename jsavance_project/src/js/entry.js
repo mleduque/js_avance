5a9ec71d800e7b64900fa2315c7fd8e72a870238
Mickaël Leduque <mickael.leduque@serli.com>
2015-10-11 15:59:04 +0200

    Created Animal constructor

--- jsavance_project/src/js/entry.js
+++ jsavance_project/src/js/entry.js
@@ -1,6 +1,3 @@
 require('../css/style.css');
 
-var greetingGenerator = require('../es6/greeting.es6');
-var content = greetingGenerator();
-var body = document.querySelector('body');
-body.appendChild(content);
+var petstore = require('./petstore');
---
+++ jsavance_project/src/js/petstore.js
@@ -0,0 +1,9 @@
+
+function Animal(id, name, species, race, age) {
+  this.id = id;
+  this.name = name;
+  this.race = race;
+  this.age = age;
+}
+
+module.exports = {Animal: Animal}
