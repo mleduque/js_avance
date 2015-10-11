1d0abd5166b27b05e89a7180d64cbdd15e99ed74
Mickaël Leduque <mickael.leduque@serli.com>
2015-10-11 16:12:29 +0200

    Create inventory module

--- jsavance_project/src/js/petstore.js
+++ jsavance_project/src/js/petstore.js
@@ -6,4 +6,45 @@ function Animal(id, name, species, race, age) {
   this.age = age;
 }
 
-module.exports = {Animal: Animal}
+var inventory = function() {
+  var animals = [];
+  var counter = 0;
+  function addAnimal(name, species, race, age) {
+    animals.push(new Animal(counter, name, species, race, age));
+    counter++;
+  }
+  function removeAnimal(id) {
+    var i;
+    for (i = 0; i < animals.length; i++) {
+      if (animals[i] === id) {
+        animals.splice(i, 1); // remove the element
+      }
+    }
+  }
+  function getAnimal(id) {
+    var i;
+    for (i = 0; i < animals.length; i++) {
+      if (animals[i] === id) {
+        return animals[id];
+      }
+    }
+  }
+  function findAnimalWithSpecies(species) {
+    var result = [];
+    var i;
+    for (i = 0; i < animals.length; i++) {
+      if (animals[i].species === species) {
+        result.push(animals[i]);
+      }
+    }
+    return result;
+  }
+
+  return {  addAnimal: addAnimal,
+            removeAnimal: removeAnimal,
+            getAnimal: getAnimal,
+            findAnimalWithSpecies: findAnimalWithSpecies
+          }
+}();
+
+module.exports = {Animal: Animal, inventory: inventory}
